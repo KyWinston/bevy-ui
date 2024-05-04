49c0a78053405e9da2004f04454fd6f4c773d012
KyleWinston <kyle.w.r@hotmail.com>
2024-05-04 19:47:19 -0400

    add slider

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -2,18 +2,23 @@ use bevy::prelude::*;
 
 use crate::UiState;
 
-use self::systems::{build_settings, spawn_settings};
+use self::{
+    resources::SettingsVals,
+    systems::{insert_setting, spawn_settings},
+};
 
 pub mod components;
 pub mod resources;
 pub mod styles;
 pub mod systems;
+pub mod events;
 
 pub struct SettingsPlugin;
 
 impl Plugin for SettingsPlugin {
     fn build(&self, app: &mut App) {
-        // app.insert_resource(RenderSettings { brightness: 1.0 })
-        app.add_systems(OnEnter(UiState::Settings), spawn_settings);
+        app.insert_resource::<SettingsVals>(SettingsVals(vec![]))
+            .add_systems(OnEnter(UiState::Settings), spawn_settings)
+            .add_systems(Startup, insert_setting);
     }
 }
